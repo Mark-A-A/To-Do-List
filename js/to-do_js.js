a3618c2c099c384bfbac8258482f8aed428567c9
Mark-A-A <markawai01@gmail.com>
2015-11-24 18:30:37 -0500

    fixed bootstrap responsiveness, add button function

--- js/to-do_js.js
+++ js/to-do_js.js
@@ -1,11 +1,19 @@
 $(document).ready(function (){
 
   $(".check-button").on("click", function(){
-    alert("Crossing It Out");
+    
+    // add strikethrough css when checked off
+
   })
 
 
   $(".btn-success").on("click", function(){
+    // append row to the table
+    var addText = $("#task").val();
+    var checkBox = "<input type=\"checkbox\" aria-label=\"...\">"; 
+    var deleteButton = "<button type=\"button\" class=\"btn btn-danger\">Delete</button>";
+    
+    $('tbody').append("<tr><td class='text-center'>" + checkBox + "</td><td class='text-center'>"+addText+"</td><td class='text-center'>"+deleteButton+"</td></tr>");
     alert("Adding Row");
   })
 
